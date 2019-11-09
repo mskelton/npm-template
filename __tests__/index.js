9133d0a949cf21167108d095b884da82a66f9ef2
Mark Skelton <mdskelton99@gmail.com>
2019-11-09 15:21:14 -0600

    Rename main generator, start tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -2,14 +2,41 @@ const path = require('path')
 const assert = require('yeoman-assert')
 const helpers = require('yeoman-test')
 
-describe('Base generator', () => {
+const defaultPrompts = {
+  projectDescription: 'Some description of my project',
+  projectId: 'my-project',
+  projectName: 'My Project',
+}
+
+describe('Base template', () => {
   beforeAll(() => {
     return helpers
-      .run(path.join(__dirname, '../generators/app'))
-      .withPrompts({ someAnswer: true })
+      .run(path.join(__dirname, '..', 'generators', 'app'))
+      .withPrompts(defaultPrompts)
   })
 
   it('creates files', () => {
-    assert.file(['dummyfile.txt'])
+    assert.file([
+      '.all-contributorsrc',
+      '.gitignore',
+      'LICENSE',
+      'package.json',
+      'README.md',
+      'yarn.lock',
+    ])
+  })
+
+  it('', () => {})
+})
+
+describe('VS Code template', () => {
+  beforeAll(() => {
+    return helpers
+      .run(path.join(__dirname, '..', 'generators', 'app'))
+      .withPrompts(defaultPrompts)
+  })
+
+  it('creates', () => {
+    assert.file(['.github/workflows/publish.yml', '.vscode/launch.json'])
   })
 })
